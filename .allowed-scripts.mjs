dd693f76905effb718897ca81ba388ca51fefdf1
Amardeep Chimber <amardeep.chimber@justice.gov.uk>
2026-06-15 13:46:47 +0100

    MAN-2799: update allowlist

--- .allowed-scripts.mjs
+++ .allowed-scripts.mjs
@@ -2,11 +2,13 @@ import { configureAllowedScripts } from '@ministryofjustice/hmpps-npm-script-all
 
 export default configureAllowedScripts({
   allowlist: {
+    // Needed by esbuild for watching files during development
+    'node_modules/@parcel/watcher@2.5.6': 'ALLOW',
+    // ESBuild is written in GoLang - this is needed to download prebuilt binaries for the specific platform
+    'node_modules/esbuild@0.28.0': 'ALLOW',
     // Needed by jest for running tests in watch mode
     'node_modules/fsevents@2.3.3': 'ALLOW',
     // Native solution to quickly resolve module paths, used by jest and eslint
-    'node_modules/unrs-resolver@1.11.1': 'ALLOW',
-    // Needed by esbuild for watching files during development
-    'node_modules/@parcel/watcher@2.5.6': 'ALLOW',
+    'node_modules/unrs-resolver@1.12.2': 'ALLOW',
   },
 })
