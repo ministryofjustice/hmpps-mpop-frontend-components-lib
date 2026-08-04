331e1443468ebc93fd67fec4a7b34ca4eab4d832
Luca Sanz <luca.sanz@justice.gov.uk>
2026-08-04 10:28:12 +0100

    [MAN-3215]: addressing security issue found in scan

--- .allowed-scripts.mjs
+++ .allowed-scripts.mjs
@@ -5,7 +5,7 @@ export default configureAllowedScripts({
     // Needed by esbuild for watching files during development
     'node_modules/@parcel/watcher@2.5.6': 'ALLOW',
     // ESBuild is written in GoLang - this is needed to download prebuilt binaries for the specific platform
-    'node_modules/esbuild@0.28.0': 'ALLOW',
+    'node_modules/esbuild@0.28.1': 'ALLOW',
     // Needed by jest for running tests in watch mode
     'node_modules/fsevents@2.3.3': 'ALLOW',
     // Native solution to quickly resolve module paths, used by jest and eslint
